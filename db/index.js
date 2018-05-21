9acd35d52ac43666047da93e165fd7e9cbd8e1dd
TiagoAndreAlmeida <thyagoeap@gmail.com>
2018-05-21 16:18:21 -0300

    Update index.js

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@
 
 const { Client } = require('pg');
 
-const connectionString = 'postgresql://postgres:root@localhost:5432/eventoFT';
+const connectionString = 'postgres://zhetgorzqrgcxa:2d1926959c9d1f8fa066db0fe2619651d51407ccf532bfd449e3a530038000ec@ec2-23-21-129-50.compute-1.amazonaws.com:5432/da6i9aso20garc';
 
 const client = new Client({connectionString});
 
